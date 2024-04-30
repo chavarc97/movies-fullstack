233257ad70be287ba35eea16753e767e93d40997
Salvador Rodriguez <chava.crb@gmail.com>
2024-04-30 16:50:28 -0600

    complete signIn/Up UI and header UI

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className="bg-transparent backdrop-blur-md shadow-md w-full fixed top-0 left-0 z-10">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to={'/'}>
+          <h1 className="font-bold underline-offset-8  text-neutral-100  text-sm sm:text-lg flex flex-wrap">
+            Movie<span className="text-cyan-600">Mate</span>
+          </h1>
+        </Link>
+        <form className=" p-3 ml-auto flex flex-wrap items-center border-b border-neutral-400">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64 "
+          />
+          <button>
+            <FaSearch className="text-neutral-600" />
+          </button>
+        </form>
+        <ul className="flex gap-4 pl-6">
+          <Link to={"/"}>
+            <li className=" hidden sm:inline text-neutral-900 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to={"/about"}>
+            <li className=" hidden sm:inline text-neutral-900 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to={"/about"}>
+            <li className=" hidden sm:inline text-neutral-900 hover:underline">
+              Profile
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,63 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
-export default SignUp
+    <main className="w-full h-screen flex items-start">
+      <div className="  sm:relative w-1/2 h-full flex flex-col">
+        <div className="absolute top-[25%] left-[10%]">
+          <h1 className="text-[#E0E0E0] font-bold text-4xl">
+            Your Perfect Movie Companion
+          </h1>
+          <p className="mt-3 text-lg text-white font font-normal">
+            Start for free and get attractive offers from community
+          </p>
+        </div>
+        <img
+          src="/cover_foto.jpg"
+          alt=""
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className=" shadow-white shadow-xl drop-shadow-2xl w-1/2 h-full bg-[#E0E0E0] flex flex-col p-20 justify-between">
+        <h1 className="text-2xl text-[060606] mt-10">
+          Movie<span className="text-cyan-600 font-bold">Mate</span>
+        </h1>
+
+        <form className=" flex flex-col mt-10 w-2/3 mx-auto">
+          <h3 className="text-xl font-semibold mb-3 ">Sign up</h3>
+          <p className="text-sm  mb-2">Welcome! Please enter your credential</p>
+
+          <input
+            type="text"
+            placeholder="username"
+            id="username"
+            className="mt-5 border-b border-neutral-400 bg-transparent focus:outline-none"
+          />
+          <input
+            type="email"
+            placeholder="email"
+            id="email"
+            className="mt-5 border-b border-neutral-400 bg-transparent focus:outline-none"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            id="password"
+            className="mt-5 border-b border-neutral-400 bg-transparent focus:outline-none"
+          />
+          <button className="mt-10 p-3 bg-neutral-800 text-white rounded">
+            Sign Up
+          </button>
+        </form>
+
+        <div className="w-full flex flex-wrap gap-2 text-sm">
+          <p>Already have an account?</p>
+          <Link className="text-cyan-600 font-semibold underline-offset-2">
+            Sign in here
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default SignUp;
